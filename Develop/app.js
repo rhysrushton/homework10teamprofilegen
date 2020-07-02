38fbd1912bce8a9a91f3eea0c623c895afb8663a
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-02 12:44:48 +1000

    css = done. reame = nearly done. html main template = done

--- Develop/app.js
+++ Develop/app.js
@@ -10,35 +10,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-//Inquirer Question for Type of EMployee. 
-
-//This is the array that will take the value from the inquirer 
+//This is an ampty array. 
+//The instances of the employees we are adding will be pushed here. 
+//This array will then be used in the render function. 
 employees = []; 
 
+//All of the below are the inquirer questions that will be 
+//referenced in the different functions. 
+
+//This is an a 'confirm' type inquirer question. 
+//This will give a Y/N option which will be useful to 
+//choose when we want to stop/continue adding team memebers. 
 const addTeam = [
     {
         type: "confirm", 
@@ -47,6 +29,7 @@ const addTeam = [
     }
 ]
 
+//These are the choices given when the user is adding team members. 
 const choices = [
     {
         type: "list",
@@ -64,25 +47,25 @@ const internQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is the name?"
+        message: "What is the intern's name?"
     },
 
     {
         type: "input",
         name: "id",
-        message: "What is the id?"
+        message: "What is the intern's id?"
     },
 
     {
         type: "input",
         name: "school",
-        message: "What is the school?"
+        message: "What is the intern's school?"
     },
 
     {
         type: "input",
         name: "email",
-        message: "What is the email?"
+        message: "What is the intern's email?"
     },
 ]
 
@@ -91,25 +74,25 @@ const engineerQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is the name?"
+        message: "What is the engineer's name?"
     },
 
     {
         type: "input",
         name: "id",
-        message: "What is the id?"
+        message: "What is the engineer's id?"
     },
 
     {
         type: "input",
         name: "email",
-        message: "What is the email?"
+        message: "What is the engineer's email?"
     },
 
     {
         type: "input",
         name: "github",
-        message: "What is the engineer github?"
+        message: "What is the engineer's github?"
     },
 ]
 
@@ -118,51 +101,54 @@ const managerQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is the name?"
+        message: "What is the manager's name?"
     },
 
     {
         type: "input",
         name: "id",
-        message: "What is the id?"
+        message: "What is the manager's id?"
     },
 
     {
         type: "input",
         name: "email",
-        message: "What is the email?"
+        message: "What is the manager's email?"
     },
 
     {
         type: "input",
         name: "officeNumber",
-        message: "What is the phone number?"
+        message: "What is the phone manager's number?"
     },
 ]
 
 //function for intern
+//uses the employee class and extends it
 function newIntern(intern){
     const { name, id, school, email } = intern;
     const currentIntern = new Intern(name, id, school, email); 
-    console.log("Intern =", intern)
+    //console.log("Intern =", intern)
     employees.push(currentIntern);
     create(); 
 }
 
 //function for engineer
+//uses the employee class and extends it
 function newEngineer(engineer){
         const {name, id, email, github} = engineer; 
         const currentEngineer = new Engineer(name, id, email, github);
-        console.log("Engineer =", engineer)
+        //console.log("Engineer =", engineer)
         employees.push(currentEngineer);
         create()
     }
 
 //function for manager. Then calls the async choice function.
+//uses the employee class and extends it
     inquirer.prompt(managerQuestions).then((manager)=>{
         const {name, id, email, officeNumber} = manager; 
         const currentManager = new Manager (name, id, email, officeNumber)
-        console.log("Manager =", manager)
+       // console.log("Manager =", manager)
         employees.push(currentManager);
         create();   
     })
@@ -183,7 +169,6 @@ async function create() {
             newEngineer(engineer); 
             console.log("E")
             break; 
-
             case "Intern":
                 const intern = await inquirer.prompt(internQuestions);
                 newIntern(intern); 
@@ -197,6 +182,7 @@ async function create() {
     } 
 }
 
+//This creates the html page
 function makePage(htmlRender){
     fs.writeFile(outputPath, htmlRender, (err) => {
         if (err) throw err; 
